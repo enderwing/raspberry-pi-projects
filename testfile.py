28a09c97f29ae01feccda0895c716acbf3b3deff
Ben Trainum <bentrainum@Bens-MacBook-Air.local>
2017-11-06 14:33:09 -0500

    another test commit

--- testfile.py
+++ testfile.py
@@ -2,4 +2,10 @@ import random as r
 import time
 print(r.random())
 time.sleep(.3)
-print(r.random())
+#removing something
+print("THis is some new stuff")
+def this_is_a_function(text):
+    print(text)
+
+#call a function
+this_is_a_function("some text")
